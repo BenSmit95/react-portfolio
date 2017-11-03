5044747dd29f59e42840d54f1513b8262566e8cf
Benjamin Smit <benjismit@live.nl>
2017-11-03 13:29:35 +0100

    add server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test' })
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.development' })
+}
+
 module.exports = (env) => {
     const isProduction = env === 'production';
     // make new ExtractTextPlugin and give it the name of the bundle file
