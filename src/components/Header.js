266c02ef5d663c8437f9f04fa63cface0eaecd0e
Benjamin Smit <benjismit@live.nl>
2017-11-05 11:32:42 +0100

    improve header mechanics

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,14 +10,23 @@ class Header extends React.Component {
         })
         history.pushState({}, "Landing", `/#${element}`);
     }
+    onScroll = () => {
+        const scrollPosition = window.scrollY;
+        const headerPos = document.getElementById('introduction').getBoundingClientRect();
+        if (scrollPosition > headerPos.top) {
+            document.querySelector('.header').classList.add('header--visible');
+        } else {
+            document.querySelector('.header').classList.remove('header--visible');
+        }
+    }
     render() {
         return (
             <header className="header">
                 <nav>
                     <ul>
-                        <a className="header__nav-link" name="landing" onClick={this.handleClick}>Landing</a>
+                        <a className="header__nav-link hidden__mobile-only" name="landing" onClick={this.handleClick}>Landing</a>
                         <a className="header__nav-link" name="introduction" onClick={this.handleClick}>Introduction</a>
-                        <a className="header__nav-link" name="hobbies" onClick={this.handleClick}>Hobbies</a>
+                        <a className="header__nav-link hidden__mobile-only" name="hobbies" onClick={this.handleClick}>Hobbies</a>
                         <a className="header__nav-link" name="skills" onClick={this.handleClick}>Skills</a>
                         <a className="header__nav-link" name="projects" onClick={this.handleClick}>Projects</a>
                         <a className="header__nav-link" name="contact" onClick={this.handleClick}>Contact</a>
@@ -25,6 +34,9 @@ class Header extends React.Component {
                 </nav>
             </header>   
         )
+    }
+    componentDidMount() {
+        document.addEventListener('scroll', this.onScroll);
     }
 }
 
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,25 +6,12 @@ import Introduction from './Introduction';
 import Skills from './Skills';
 
 class MainPage extends React.Component {
-    state = {
-        showHeader: false
-    }
-
-    onScroll = () => {
-        const scrollPosition = window.scrollY;
-        const headerPos = document.getElementById('introduction').getBoundingClientRect();
-        if (scrollPosition > headerPos.top) {
-            this.setState(() => ({
-                showHeader: true
-            }));
-        }
-    }
 
     render() {
         return (
             <div>
                 <Landing />
-                {this.state.showHeader && <Header />}
+                <Header />
                 <main>
                     <Introduction />
                     <Hobbies />
@@ -34,9 +21,6 @@ class MainPage extends React.Component {
         );
     }
 
-    componentDidMount() {
-        document.addEventListener('scroll', this.onScroll);
-    }
 }
 
 export default MainPage;
