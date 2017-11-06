d8b082d9bd3bb8372f0a0a2d466a6520b5c416b8
Benjamin Smit <benjismit@live.nl>
2017-11-06 19:42:25 +0100

    add loading screen

--- src/app.js
+++ src/app.js
@@ -4,4 +4,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import MainPage from './components/MainPage.js'
 
-ReactDOM.render(<MainPage />, document.getElementById('app'));
+setTimeout(() => {
+    ReactDOM.render(<MainPage />, document.getElementById('app'));
+}, 1000);
