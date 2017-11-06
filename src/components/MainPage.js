2053a126d5f3ef633364bee264ac73ed47ca01d6
Benjamin Smit <benjismit@live.nl>
2017-11-06 15:25:52 +0100

    add skills scale effect

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -9,6 +9,13 @@ import Projects from './Projects';
 import Skills from './Skills';
 
 class MainPage extends React.Component {
+    state = {
+        introductionVisible: false,
+        hobbiesVisible: false,
+        skillsVisible: false,
+        projectsVisible: false,
+        contactVisible: false,
+    }
     addAppearClass = (sectionName) => {
         document.querySelector(`#${sectionName} .section__title`).classList.add('appear');
         document.querySelector(`#${sectionName} .section__sub-title`).classList.add('appear');
@@ -16,17 +23,58 @@ class MainPage extends React.Component {
         document.querySelectorAll(`#${sectionName} .section__header-border`)[1].classList.add('appear__border');
     }
     onScroll = () => {
-        const introductionY = document.getElementById('introduction').getBoundingClientRect().top;
-        const hobbiesY = document.getElementById('hobbies').getBoundingClientRect().top;
-        const skillsY = document.getElementById('skills').getBoundingClientRect().top;
-        const projectsY = document.getElementById('projects').getBoundingClientRect().top;
-        const contactY = document.getElementById('contact').getBoundingClientRect().top;
-
-        if (introductionY < 700) this.addAppearClass('introduction');
-        if (hobbiesY < 700) this.addAppearClass('hobbies');
-        if (skillsY < 700) this.addAppearClass('skills');
-        if (projectsY < 700) this.addAppearClass('projects');
-        if (contactY < 1000) this.addAppearClass('contact');
+        if (!this.state.introductionVisible) {
+            const introductionY = document.getElementById('introduction').getBoundingClientRect().top;
+            if (introductionY < 500) {
+                this.setState(() => ({
+                    introductionVisible: true
+                }));
+                this.addAppearClass('introduction');
+            }
+        }
+        if (!this.state.hobbiesVisible) {
+            const hobbiesY = document.getElementById('hobbies').getBoundingClientRect().top;
+            if (hobbiesY < 500) {
+                this.setState(() => ({
+                    hobbiesVisible: true
+                }));
+                this.addAppearClass('hobbies');
+            }
+        }
+        if (!this.state.skillsVisible) {
+            const skillsY = document.getElementById('skills').getBoundingClientRect().top;
+            if (skillsY < 500) {
+                this.setState(() => ({
+                    skillsVisible: true
+                }));
+                this.addAppearClass('skills');
+                const skillsList = document.getElementsByClassName('skills__list-item');
+                const timeBetweenAppears = 300;
+                for (let i = 0; i < skillsList.length; i++) {
+                    setTimeout(() => {
+                        skillsList[i].classList.add('skills__list-item--appear');
+                    }, i * timeBetweenAppears + 1500)
+                }
+            }
+        }
+        if (!this.state.projectsVisible) {
+            const projectsY = document.getElementById('projects').getBoundingClientRect().top;
+            if (projectsY < 500) {
+                this.setState(() => ({
+                    projectsVisible: true
+                }));
+                this.addAppearClass('projects');
+            }
+        }
+        if (!this.state.contactVisible) {
+            const contactY = document.getElementById('contact').getBoundingClientRect().top;
+            if (contactY < 800) {
+                this.setState(() => ({
+                    contactVisible: true
+                }));
+                this.addAppearClass('contact');
+            }
+        }
     }
 
     render() {
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,79 +15,75 @@ import { LogoHtml,
     LogoWebpack
  } from './logos/Logos';
 
-class Skills extends React.Component {
-    render() {
-        return (
-            <section id="skills" className="section">
-                <div className="container container--large">
-                    <div className="section__header-container">
-                        <hr className="section__header-border" />
-                        <h2 className="section__title">Skills</h2>
-                        <h3 className="section__sub-title">The tools and technologies I have experience with</h3>
-                        <hr className="section__header-border" />
-                    </div>
-                    <ul className="skills__list">
-                        <li className="skills__list-item">
-                            <p>HTML5</p>
-                            <LogoHtml />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>CSS3</p>
-                            <LogoCSS />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>ES6</p>
-                            <LogoJS />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>jQuery</p>
-                            <LogoJQuery />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>Sass</p>
-                            <LogoSass />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>NodeJS</p>
-                            <LogoNodeJS />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>ReactJS</p>
-                            <LogoReact />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>Redux</p>
-                            <LogoRedux />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>Java</p>
-                            <LogoJava />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>CSharp</p>
-                            <LogoCSharp />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>Git</p>
-                            <LogoGit />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>GitHub</p>
-                            <LogoGitHub />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>Heroku</p>
-                            <LogoHeroku />
-                        </li>
-                        <li className="skills__list-item">
-                            <p>Webpack</p>
-                            <LogoWebpack />
-                        </li>
-                    </ul>
-                </div>
-            </section>
-        )
-    }  
-}
+const Skills = () => (
+    <section id="skills" className="section">
+        <div className="container container--large">
+            <div className="section__header-container">
+                <hr className="section__header-border" />
+                <h2 className="section__title">Skills</h2>
+                <h3 className="section__sub-title">The tools and technologies I have experience with</h3>
+                <hr className="section__header-border" />
+            </div>
+            <ul className="skills__list">
+                <li className="skills__list-item">
+                    <p>HTML5</p>
+                    <LogoHtml />
+                </li>
+                <li className="skills__list-item">
+                    <p>CSS3</p>
+                    <LogoCSS />
+                </li>
+                <li className="skills__list-item">
+                    <p>ES6</p>
+                    <LogoJS />
+                </li>
+                <li className="skills__list-item">
+                    <p>jQuery</p>
+                    <LogoJQuery />
+                </li>
+                <li className="skills__list-item">
+                    <p>Sass</p>
+                    <LogoSass />
+                </li>
+                <li className="skills__list-item">
+                    <p>NodeJS</p>
+                    <LogoNodeJS />
+                </li>
+                <li className="skills__list-item">
+                    <p>ReactJS</p>
+                    <LogoReact />
+                </li>
+                <li className="skills__list-item">
+                    <p>Redux</p>
+                    <LogoRedux />
+                </li>
+                <li className="skills__list-item">
+                    <p>Java</p>
+                    <LogoJava />
+                </li>
+                <li className="skills__list-item">
+                    <p>CSharp</p>
+                    <LogoCSharp />
+                </li>
+                <li className="skills__list-item">
+                    <p>Git</p>
+                    <LogoGit />
+                </li>
+                <li className="skills__list-item">
+                    <p>GitHub</p>
+                    <LogoGitHub />
+                </li>
+                <li className="skills__list-item">
+                    <p>Heroku</p>
+                    <LogoHeroku />
+                </li>
+                <li className="skills__list-item">
+                    <p>Webpack</p>
+                    <LogoWebpack />
+                </li>
+            </ul>
+        </div>
+    </section>
+)
 
 export default Skills;
